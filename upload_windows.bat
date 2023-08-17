e803a986dca524cdbf12d77cc438a351429bdd1d
Cinghy Creations <cinghycreations@gmail.com>
2023-08-17 16:55:49 +0200

    Fixed upload scripts for Windows and HTML5

--- upload_windows.bat
+++ upload_windows.bat
@@ -1,2 +1,4 @@
-butler push build temp\build\windows\Release cinghycreations/destructive-drones:windows
+set PATH=%PATH%;C:\Projects\destructive_drones\temp\butler
+robocopy /e build temp\build\windows\Release
+butler push temp\build\windows\Release cinghycreations/destructive-drones:windows
 pause
